e80be01e7a98b77543aa6a792d8f3bb994c611f5
sathya-pramodh <sathyapramodh17@gmail.com>
2024-06-24 19:50:08 +0530

    website: Update deps and some renaming cleanup.

--- website/src/pages/BookingPage.tsx
+++ website/src/pages/BookingPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Select, { SingleValue } from 'react-select';
+import Select, { SingleValue, StylesConfig } from 'react-select';
 import TitlePanel from "../components/TitlePanel";
 
 interface OptionType {
@@ -17,24 +17,24 @@ const options: OptionType[] = [
     { value: 'Status of The Train', label: 'Status of The Train', description: "Status of The Train" }
 ];
 
-const customStyles = {
-    menu: (provided: any) => ({
+const customStyles: StylesConfig<OptionType, false> = {
+    menu: (provided) => ({
         ...provided,
         zIndex: 9999,
         backgroundColor: 'black',
         color: 'white'
     }),
-    control: (provided: any) => ({
+    control: (provided) => ({
         ...provided,
         backgroundColor: 'black',
         color: 'white',
         border: '1px solid white'
     }),
-    singleValue: (provided: any) => ({
+    singleValue: (provided) => ({
         ...provided,
         color: 'white'
     }),
-    option: (provided: any, state: { isFocused: any; }) => ({
+    option: (provided, state: { isFocused: boolean; }) => ({
         ...provided,
         backgroundColor: state.isFocused ? 'grey' : 'black',
         color: 'white'
@@ -45,12 +45,12 @@ const BookingPage = () => {
     const [selectedOption, setSelectedOption] = useState<OptionType | null>(null);
     const [sourceDestination, setSourceDestination] = useState('');
     const [finalDestination, setFinalDestination] = useState('');
-    const [Date, setDate] = useState('');
-    const [Time, setTime] = useState('');
+    const [date, setDate] = useState('');
+    const [time, setTime] = useState('');
     const [priceRange, setPriceRange] = useState('');
-    const [Train, setTrain] = useState('');
-    const [AccountID, setAccountID] = useState('');
-    const [STrainID, setSTrainID] = useState('');
+    const [train, setTrain] = useState('');
+    const [accountID, setAccountID] = useState('');
+    const [sTrainID, setSTrainID] = useState('');
 
 
     const handleSelectChange = (option: SingleValue<OptionType>) => {
@@ -72,15 +72,14 @@ const BookingPage = () => {
         // This function can be expanded based on your application's requirements
         console.log('Source Destination:', sourceDestination);
         console.log('Final Destination:', finalDestination);
-        console.log('Date:', Date);
-        console.log('Time:', Time);
+        console.log('Date:', date);
+        console.log('Time:', time);
         console.log('PriceRange:', priceRange);
-        console.log('Account Id:', AccountID);
-        console.log('Train Name or Id:', Train);
-        console.log('Status  TrainId:', STrainID);
-        
+        console.log('Account Id:', accountID);
+        console.log('Train Name or Id:', train);
+        console.log('Status  TrainId:', sTrainID);
+
     };
-    
 
     return (
         <>
@@ -90,13 +89,12 @@ const BookingPage = () => {
             <div className="relative z-10 flex items-center justify-center min-h-screen">
                 <div className="w-full max-w-md p-4 bg-black text-white shadow-md rounded-lg text-center">
                     <h2 className="mt-4 text-lg">Select your Facility</h2>
-                    <Select 
+                    <Select
                         options={options}
                         onChange={handleSelectChange}
                         placeholder="Select an option"
                         styles={customStyles} // Apply custom styles
-                        className="mt-4"
-                        classNamePrefix="react-select"
+                        className="react-select mt-4"
                     />
                     {selectedOption && selectedOption.value === 'Destination' && (
                         <div className="mt-4 p-4 bg-gray-800 rounded-lg">
@@ -134,7 +132,7 @@ const BookingPage = () => {
                                 <label className="block text-white">Date:</label>
                                 <input
                                     type="date"
-                                    value={Date}
+                                    value={date}
                                     onChange={(e) => setDate(e.target.value)}
                                     className="block w-full px-3 py-2 mt-1 text-white bg-gray-700 rounded-md focus:outline-none focus:ring"
                                 />
@@ -143,7 +141,7 @@ const BookingPage = () => {
                                 <label className="block text-white">Time in 24hrs format:</label>
                                 <input
                                     type="time"
-                                    value={Time}
+                                    value={time}
                                     onChange={(e) => setTime(e.target.value)}
                                     className="block w-full px-3 py-2 mt-1 text-white bg-gray-700 rounded-md focus:outline-none focus:ring"
                                 />
@@ -158,26 +156,26 @@ const BookingPage = () => {
                     )}
                     {selectedOption && selectedOption.value === 'Price' && (
                         <div className="mt-4 p-4 bg-gray-800 rounded-lg">
-        
+
                             <div className="mt-2">
-                            <label className="block text-white">Price Range:</label>
-                            <select
-                                value={priceRange}
-                                onChange={(e) => setPriceRange(e.target.value)}
-                                className="block w-full px-3 py-2 mt-1 text-white bg-gray-700 rounded-md focus:outline-none focus:ring"
-                            >
-                                <option value="<1000">Less than Rs1000</option>
-                                <option value="1000-2500">Rs1000 - Rs2500</option>
-                                <option value="2500-5000">Rs2500 - Rs5000</option>
-                                <option value=">5000">Beyond Rs5000</option>
-                            </select>
+                                <label className="block text-white">Price Range:</label>
+                                <select
+                                    value={priceRange}
+                                    onChange={(e) => setPriceRange(e.target.value)}
+                                    className="block w-full px-3 py-2 mt-1 text-white bg-gray-700 rounded-md focus:outline-none focus:ring"
+                                >
+                                    <option value="<1000">Less than Rs1000</option>
+                                    <option value="1000-2500">Rs1000 - Rs2500</option>
+                                    <option value="2500-5000">Rs2500 - Rs5000</option>
+                                    <option value=">5000">Beyond Rs5000</option>
+                                </select>
                             </div>
                             <button
                                 onClick={handleSubmit}
                                 className="mt-4 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md focus:outline-none"
                             >
-                                 Submit
-                                </button>
+                                Submit
+                            </button>
                         </div>
                     )}
                     {selectedOption && selectedOption.value === 'Train' && (
@@ -187,7 +185,7 @@ const BookingPage = () => {
                                 <label className="block text-white">Train Name or Id:</label>
                                 <input
                                     type="text"
-                                    value={Train}
+                                    value={train}
                                     onChange={(e) => setTrain(e.target.value)}
                                     className="block w-full px-3 py-2 mt-1 text-white bg-gray-700 rounded-md focus:outline-none focus:ring"
                                 />
@@ -207,7 +205,7 @@ const BookingPage = () => {
                                 <label className="block text-white">Enter the AccountID:</label>
                                 <input
                                     type="text"
-                                    value={AccountID}
+                                    value={accountID}
                                     onChange={(e) => setAccountID(e.target.value)}
                                     className="block w-full px-3 py-2 mt-1 text-white bg-gray-700 rounded-md focus:outline-none focus:ring"
                                 />
@@ -227,7 +225,7 @@ const BookingPage = () => {
                                 <label className="block text-white">Enter the TrainId:</label>
                                 <input
                                     type="number"
-                                    value={STrainID}
+                                    value={sTrainID}
                                     onChange={(e) => setSTrainID(e.target.value)}
                                     className="block w-full px-3 py-2 mt-1 text-white bg-gray-700 rounded-md focus:outline-none focus:ring"
                                 />
